723fe219c4d872204f4c879ed803b0c128ebcdd4
johannaSommer <johanna@mail-sommer.com>
2023-11-18 12:08:30 +0100

    Upgrade utils to new package name

--- models/global_utils.py
+++ models/global_utils.py
@@ -6,6 +6,7 @@ from typing import Union
 
 from rdkit import Chem
 
+
 # To be adjusted by user
 config = dict(
     SEML_CONFIG_FILE="/path/to/seml/config",
@@ -13,17 +14,17 @@ config = dict(
     WB_PROJECT="molecule-generation",
     WB_ENTITY="molgen",
 )
-
 SEML_CONFIG_PATH = Path(config["SEML_CONFIG_FILE"])
 SOURCE_DIR = Path(config["SOURCE_DIR"])
 BASELINE_DIR = SOURCE_DIR / "project_dir"
 DATA_DIR = BASELINE_DIR / "data"
+config["BASELINE_DIR"] = str(BASELINE_DIR)
+
 SUPPORTED_MODELS = [
     "JTVAE",
     "PSVAE",
     "SMILESLSTM",
     "HIERVAE",
-    "MICAM",
     "GCPN",
     "GRAPHAF",
     "MOLER",
@@ -36,7 +37,7 @@ TENSORFLOW_MODELS = ["CHARVAE", "MOLER"]
 
 def get_all_model_funcs(model_name: str) -> dict:
     handle_tensorflow(model_name)
-    module = importlib.import_module(f"baselines.{model_name.lower()}.wrapper_utils")
+    module = importlib.import_module(f"models.{model_name.lower()}.wrapper_utils")
     return dict(
         train=partial(module.run_training, config=config),
         load=partial(module.get_model_func, config=config),
@@ -52,7 +53,7 @@ def smiles_from_file(file_path: Union[str, Path]):
 
 
 def get_model_config(config, model_name, dataset):
-    path = Path(config["SOURCE_DIR"]) / "baselines" / model_name.lower() / "model_config.json"
+    path = Path(config["SOURCE_DIR"]) / "models" / model_name.lower() / "model_config.json"
     params = json.load(open(path, "r"))[dataset]
     return params
 
